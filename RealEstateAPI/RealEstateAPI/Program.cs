05d14f06918c91dff371fbb6b52d7ad7a2f03ae6
aleveri-jasetech <andres@jasetech.ca>
2025-08-25 12:55:38 -0500

    CORS support

--- RealEstateAPI/RealEstateAPI/Program.cs
+++ RealEstateAPI/RealEstateAPI/Program.cs
@@ -28,6 +28,18 @@ builder.Services.AddScoped<IPropertyService, PropertyService>();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -38,6 +50,8 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
